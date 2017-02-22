e512c40e959fbb6537b108662ed19a401f57bb79
賴勁福 <freelife1018@yahoo.com.tw>
2017-02-22 11:27:06 +0800

    Signed-off-by: 賴勁福 <freelife1018@yahoo.com.tw>

--- GSStravel/src/main/java/controller/FamilyServlet.java
+++ GSStravel/src/main/java/controller/FamilyServlet.java
@@ -18,8 +18,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import org.apache.el.parser.BooleanNode;
 import org.json.simple.JSONObject;
 
 import ch.qos.logback.core.net.SyslogOutputStream;
