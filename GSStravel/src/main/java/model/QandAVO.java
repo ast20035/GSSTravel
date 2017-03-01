e86d5a4047d23039de4c5114c7c056ed046d211b
張雅婷 <ast20035@localhost>
2017-03-01 14:30:40 +0800

    新增QandA相關的檔案

---
+++ GSStravel/src/main/java/model/QandAVO.java
@@ -0,0 +1,67 @@
+package model;
+
+public class QandAVO {
+	private String tra_No;
+	private int question_No;
+	private int qa_No;
+	private String question_Time;
+	private String question_Text;
+	private String question_Title;
+	private int answer_No;
+	private String answer_Text;
+	private String answer_Time;
+	public String getTra_No() {
+		return tra_No;
+	}
+	public void setTra_No(String tra_No) {
+		this.tra_No = tra_No;
+	}
+	public int getQuestion_No() {
+		return question_No;
+	}
+	public void setQuestion_No(int question_No) {
+		this.question_No = question_No;
+	}
+	public int getQa_No() {
+		return qa_No;
+	}
+	public void setQa_No(int qa_No) {
+		this.qa_No = qa_No;
+	}
+	public String getQuestion_Time() {
+		return question_Time;
+	}
+	public void setQuestion_Time(String question_Time) {
+		this.question_Time = question_Time;
+	}
+	public String getQuestion_Text() {
+		return question_Text;
+	}
+	public void setQuestion_Text(String question_Text) {
+		this.question_Text = question_Text;
+	}
+	public String getQuestion_Title() {
+		return question_Title;
+	}
+	public void setQuestion_Title(String question_Title) {
+		this.question_Title = question_Title;
+	}
+	public int getAnswer_No() {
+		return answer_No;
+	}
+	public void setAnswer_No(int answer_No) {
+		this.answer_No = answer_No;
+	}
+	public String getAnswer_Text() {
+		return answer_Text;
+	}
+	public void setAnswer_Text(String answer_Text) {
+		this.answer_Text = answer_Text;
+	}
+	public String getAnswer_Time() {
+		return answer_Time;
+	}
+	public void setAnswer_Time(String answer_Time) {
+		this.answer_Time = answer_Time;
+	}
+}
