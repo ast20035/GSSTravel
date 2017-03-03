a7f6b58f0f642846652e52f0b3497fb0366abab3
張雅婷 <ast20035@localhost>
2017-03-03 14:59:52 +0800

    Signed-off-by: 張雅婷 <ast20035@localhost>

--- GSStravel/src/main/java/controller/QandAInsertServlet.java
+++ GSStravel/src/main/java/controller/QandAInsertServlet.java
@@ -1,7 +1,6 @@
 package controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,8 +9,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.json.simple.JSONObject;
 
 import model.EmployeeService;
 import model.EmployeeVO;
