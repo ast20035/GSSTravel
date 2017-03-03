03e3b6dac6d5b9f2f32614759374588ba69da1ec
柯皓元 <ben19931216@gmail.com>
2017-03-03 13:39:56 +0800

    Signed-off-by: 柯皓元 <ben19931216@gmail.com>

--- GSStravel/src/main/java/controller/QandAInsertServlet.java
+++ GSStravel/src/main/java/controller/QandAInsertServlet.java
@@ -1,4 +1,5 @@
 package controller;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
@@ -20,31 +21,31 @@ import model.QandAVO;
 @WebServlet("/QandAInsertServlet")
 public class QandAInsertServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		QandAService QAService =new QandAService();
-		Map<String,String> Msg = new HashMap<String, String>();
+		QandAService QAService = new QandAService();
+		Map<String, String> Msg = new HashMap<String, String>();
 		request.setAttribute("Msg", Msg);
-		String prodaction =request.getParameter("prodaction");
+		String prodaction = request.getParameter("prodaction");
 		boolean b;
-		//新增詢問
-		if("insertQuestion".equals(prodaction)){
+		// 新增詢問
+		if ("insertQuestion".equals(prodaction)) {
 			QandAVO bean = new QandAVO();
-			String temp= request.getParameter("Question_No");
-			String tra_No=request.getParameter("Select");
-			String temp2= request.getParameter("radio");
+			String temp = request.getParameter("Question_No");
+			String tra_No = request.getParameter("Select");
+			String temp2 = request.getParameter("radio");
 			String Qestion_Title = request.getParameter("Qestion_Title");
 			String Qestion_Text = request.getParameter("Qestion_Text");
 			int Question_No = Integer.parseInt(temp);
-			boolean question_secret=Boolean.parseBoolean(temp2);
-			if(Qestion_Title.trim().length()==0){
+			boolean question_secret = Boolean.parseBoolean(temp2);
+			if (Qestion_Title.trim().length() == 0) {
 				Msg.put("message", "詢問標題不可為空值");
 				request.getRequestDispatcher("/QandAServlet?prodaction=question&role=false").forward(request, response);
 				return;
 			}
-			if(Qestion_Text.trim().length()==0){
-				Msg.put("message", "詢問內容不可為空值");	
+			if (Qestion_Text.trim().length() == 0) {
+				Msg.put("message", "詢問內容不可為空值");
 				request.getRequestDispatcher("/QandAServlet?prodaction=question&role=false").forward(request, response);
 				return;
 			}
@@ -53,96 +54,89 @@ public class QandAInsertServlet extends HttpServlet {
 			bean.setQuestion_Title(Qestion_Title);
 			bean.setQuestion_Text(Qestion_Text);
 			bean.setQuestion_secret(question_secret);
-			b=QAService.insertQuestion(bean);
-			if(b){
+			b = QAService.insertQuestion(bean);
+			if (b) {
 				Msg.put("message", "詢問成功，請等候福委會通知");
-			}
-			else{
+			} else {
 				Msg.put("message", "詢問失敗");
 			}
 			request.getRequestDispatcher("/QandAServlet?role=false").forward(request, response);
 			return;
 		}
-		//新增回應、修改回應
-		if("insertAnswer".equals(prodaction)||"updateAnswer".equals(prodaction)){
+		// 新增回應、修改回應
+		if ("insertAnswer".equals(prodaction) || "updateAnswer".equals(prodaction)) {
 			QandAVO bean = new QandAVO();
 			String temp = request.getParameter("qa_No");
-			String temp2= request.getParameter("answer_No");
-			String answer_Text=request.getParameter("answer_Text");
-			int qa_No=Integer.parseInt(temp);
-			int answer_No=Integer.parseInt(temp2);
-			if(answer_Text.trim().length()==0){
+			String temp2 = request.getParameter("answer_No");
+			String answer_Text = request.getParameter("answer_Text");
+			int qa_No = Integer.parseInt(temp);
+			int answer_No = Integer.parseInt(temp2);
+			if (answer_Text.trim().length() == 0) {
 				Msg.put("message", "回應欄不可為空值");
-				request.getRequestDispatcher("/QandAServlet?prodaction=select&role=true&qa_No="+qa_No).forward(request, response);
+				request.getRequestDispatcher("/QandAServlet?prodaction=select&role=true&qa_No=" + qa_No)
+						.forward(request, response);
 				return;
 			}
 			bean.setQa_No(qa_No);
 			bean.setAnswer_No(answer_No);
 			bean.setAnswer_Text(answer_Text);
-			b=QAService.insertAnswer(bean);
-			if("insertAnswer".equals(prodaction)){
-				if(b){
+			b = QAService.insertAnswer(bean);
+			if ("insertAnswer".equals(prodaction)) {
+				if (b) {
 					Msg.put("message", "回應成功,是否要寄Email");
-				}
-				else{
+				} else {
 					Msg.put("message", "回應失敗");
 				}
-			}else{
-				if(b){
+			} else {
+				if (b) {
 					Msg.put("messageupdate", "修改成功");
-				}
-				else{
+				} else {
 					Msg.put("messageupdate", "修改失敗");
 				}
 			}
-			
-			
-			
+
 		}
-		//(Ajax)寄Email
-		if("Email".equals(prodaction)){
-			String temp= request.getParameter("qa_No");
+		// (Ajax)寄Email
+		if ("Email".equals(prodaction)) {
+			String temp = request.getParameter("qa_No");
 			QandAVO bean = new QandAVO();
 			int qa_No = Integer.parseInt(temp);
 			bean = QAService.getALL(qa_No);
-			
-			EmployeeService employeeService=new EmployeeService();
-			EmployeeVO empVO =new EmployeeVO();
-			empVO=employeeService.select(Integer.toString(bean.getQuestion_No()));
-			
-			String Question_Email=empVO.getEmp_Mail();
-			String Question_name=empVO.getEmp_Name();
-			String Question_Title =bean.getQuestion_Title();
-			
-			//內容為 
-			/*
-			 * Question_name 你好
-			 * 	你在Q&A所提出Question_Title問題福委會已回應你
-			 * 	如有問題....((後面交給你決定了~皓元老大!!
-			 * 
-			 * */
+
+			EmployeeService employeeService = new EmployeeService();
+			EmployeeVO empVO = new EmployeeVO();
+			empVO = employeeService.select(Integer.toString(bean.getQuestion_No()));
+
+			String Question_Email = empVO.getEmp_Mail();
+			String Question_name = empVO.getEmp_Name();
+			String Question_Title = bean.getQuestion_Title();
+
+			email em = new email();
+			em.send(Question_Email, "您的" + Question_Title + "問題已回覆",
+					Question_name + "您好！\n您在Q&A所提出的" + Question_Title + "問題福委會已回應您\n如有問題，請在Q&A專區留言，將會有專人回答您！");
 		}
-		//刪除單筆
-		if("deleteOne".equals(prodaction)){
+		// 刪除單筆
+		if ("deleteOne".equals(prodaction)) {
 			String temp = request.getParameter("qa_No");
-			int qa_No=Integer.parseInt(temp);
-			b=QAService.deleteOne(qa_No);
-			if(b){
+			int qa_No = Integer.parseInt(temp);
+			b = QAService.deleteOne(qa_No);
+			if (b) {
 				Msg.put("message", "刪除成功");
-			}else{
+			} else {
 				Msg.put("message", "刪除失敗");
 			}
 		}
-		//刪除一定時間內的筆數
-		if("Years".equals(prodaction)||"9month".equals(prodaction)||"6month".equals(prodaction)||"3month".equals(prodaction)){
-			b=QAService.delete(prodaction);
-			if(b){
+		// 刪除一定時間內的筆數
+		if ("Years".equals(prodaction) || "9month".equals(prodaction) || "6month".equals(prodaction)
+				|| "3month".equals(prodaction)) {
+			b = QAService.delete(prodaction);
+			if (b) {
 				Msg.put("message", "刪除成功");
-			}else{
+			} else {
 				Msg.put("message", "刪除失敗");
 			}
 		}
-		request.getRequestDispatcher("/QandAServlet?role="+request.getParameter("role")).forward(request, response);
+		request.getRequestDispatcher("/QandAServlet?role=" + request.getParameter("role")).forward(request, response);
 		return;
 	}
 
